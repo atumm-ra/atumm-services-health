030314476a98b21eb3bc20c71648e59e10a49c22
omars44 <omar@atumm.org>
2023-09-19 10:05:02 +0300

    atumm.core: class-based routing

--- atumm/services/health/entrypoints/rest/health/routers.py
+++ atumm/services/health/entrypoints/rest/health/routers.py
@@ -1,15 +1,16 @@
-from classy_fastapi import Routable, get
+from atumm.extensions.fastapi.routable import Routable, bind_router
 from fastapi import Response
+from fastapi.routing import APIRouter
+
+router = APIRouter()
 
 
+@bind_router(router)
 class HealthRouter(Routable):
     """Home router."""
 
-    def __init__(self):
-        super().__init__(prefix="")
-
-    @get("/health")
-    async def home(self):
+    @router.get("/health")
+    async def health(self):
         return Response(status_code=200)
 
 
